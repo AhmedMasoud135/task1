541e2cf80c5aa9e3087dbfa2018cc25b3bcf866b
AhmedMasoud135 <162628964+AhmedMasoud135@users.noreply.github.com>
2024-03-07 21:57:31 +0200

    Add files via upload

---
+++ Task1/AhmedMasoud1.py
@@ -0,0 +1,19 @@
+# Reverse string problem:
+
+y = input("Word = ")
+y= y[ : : -1]
+print("Reverse = ", y)
+
+
+
+
+# Factorial problem:
+
+x = int(input("Number = "))
+n=1
+for i in range(1,x+1):
+    n= n*i 
+    
+print("Factorial = ",n)
+
+
